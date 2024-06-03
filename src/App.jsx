55e923e9fd4ba38e05abaa155f23813c1e2a96b1
DexRam <105272588+LeaveThePast@users.noreply.github.com>
2024-06-03 10:04:02 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import moment from "moment";
+import "moment/locale/ru"; // Для локализации на русский язык
+
+moment.locale("ru");
+
+function DateTime(props) {
+  return <p className={props.className}>{props.date}</p>;
+}
+
+// HOC для преобразования даты
+function DateTimePretty(Component) {
+  return function (props) {
+    const { date, className } = props;
+    const formattedDate = moment(date).fromNow();
+
+    return <Component date={formattedDate} className={className} />;
+  };
+}
+
+const DateTimeEnhanced = DateTimePretty(DateTime);
+
+function Video(props) {
+  return (
+    <div className="video">
+      <iframe
+        src={props.url}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+      <DateTimeEnhanced date={props.date} className="date" />
+    </div>
+  );
+}
+
+function VideoList(props) {
+  return props.list.map((item) => <Video url={item.url} date={item.date} />);
+}
+
+export default function App() {
+  const [list, setList] = React.useState([
+    {
+      url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2017-07-31 13:24:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-03-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-02-03 23:16:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-01-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-01-01 16:17:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2017-12-02 05:24:00",
+    },
+  ]);
+
+  return <VideoList list={list} />;
+}
